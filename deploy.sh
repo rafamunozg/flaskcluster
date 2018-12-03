1780a0dd891dd5f670eaf39a468d9cde70b39caa
Rafa Munoz <rafamunozg@users.noreply.github.com>
2018-12-02 23:19:31 -0600

    Create deploy.sh
    Initial version of the deployment script to recreate the cluster environment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script will setup the environment to run a flask cluster behind a nginx LB
+
+# Clean up docker relevant instances
+docker rm -f restapp0
+docker rm -f restapp1
+
+# Now the code - Pending
+# rm -rf ~/flask_cluster/restapp
+# git clone ... 
+
+# Run docker instances with Flask
+docker run --name restapp0 --restart=always -p 8080:80 -v ~/flask-cluster/restapp:/app -d jazzdd/alpine-flask
+docker run --name restapp1 --restart=always -p 8081:80 -v ~/flask-cluster/restapp:/app -d jazzdd/alpine-flask
+
