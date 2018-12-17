3202a89ef6c82750d243689250390ec5d096c38a
Rafa Munoz <rafamunozg@users.noreply.github.com>
2018-12-17 15:26:55 -0600

    Fixing path to app.py
    Docker containers were not working properly

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ docker rm -f restapp1
 # git clone ... 
 
 # Run docker instances with Flask
-docker run --name restapp0 --restart=always -p 8080:80 -v $PWD/restapp:/app -d jazzdd/alpine-flask
-docker run --name restapp1 --restart=always -p 8081:80 -v $PWD/restapp:/app -d jazzdd/alpine-flask
+docker run --name restapp0 --restart=always -p 8080:80 -v $PWD/flaskcluster/restapp:/app -d jazzdd/alpine-flask
+docker run --name restapp1 --restart=always -p 8081:80 -v $PWD/flaskcluster/restapp:/app -d jazzdd/alpine-flask
 
 echo 'Your instances should now be running on your public IP/hostname ports 8080 and 8081 ...'
 
